fc1a6a2dbf7673fbc81b30944067b20ed97dee28
abmadani <46302888+abmadani@users.noreply.github.com>
2019-05-04 10:22:21 +0430

    put delet  on viewset

--- src/profile_project/profile_api/views.py
+++ src/profile_project/profile_api/views.py
@@ -70,6 +70,8 @@ class HelloViewSet(viewsets.ViewSet):
 	"""Test ViewsSet"""
 	
 
+	serializer_class = serializers.HelloSerializer
+
 	def list(self,request):
 		""" Return a hello message """
 
@@ -81,6 +83,43 @@ class HelloViewSet(viewsets.ViewSet):
 
 		return Response({'message':'Hello','a_viewset':a_viewset})
 
+
+	def create(self,request):
+
+		"""Create a new hello messgae"""
+
+		serializer = serializers.HelloSerializer(data=request.data)
+		if serializer.is_valid():
+			name = serializer.data.get('name')
+			message = 'hello {0}'.format(name)
+			return Response({'message':message})
+
+		else: 
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST )
+
+
+	def retrieve(self,request,pk =None):
+
+		"""handeles getting an object"""
+
+		return Response({'http_method':'GET'})
+
+
+	def update(self, request, pk=None):
+		"""pk none because we want to know wich objecting is updating"""
+		return Response({'http_method':'PUT'})	
+
+	def partial_update(self,request,pk=None):
+
+		"""Handles updating part of an object"""
+
+		return  Response({'http_method':'PATCH'})
+
+	def destroy(self, request, pk=None):
+
+		"""handels removing"""
+
+		return Response({'http_method':'DELETE'})
 
 
 
