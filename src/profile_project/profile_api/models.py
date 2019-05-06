d5b8d3324b818408db8f071ee20eb5fcb0297787
abmadani <46302888+abmadani@users.noreply.github.com>
2019-05-06 11:45:08 +0430

    add model and migration to database and admin add and updated

--- src/profile_project/profile_api/models.py
+++ src/profile_project/profile_api/models.py
@@ -71,7 +71,15 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 		return self.email
 
 
+class ProfileFeedItem(models.Model):
+	""" Status update"""
+	user_profile = models.ForeignKey('UserProfile',on_delete=models.CASCADE)#delete all of it
+	status_text = models.CharField(max_length=255)
+	create_on = models.DateTimeField(auto_now_add=True)
 
+	def __str__(self):
+		"""return a models as string"""
+		return self.status_text
 
 
 
